5dca8f51c7f0d904dbb9aaef86415d22526229f7
Austin3939 <a.blackman39@gmail.com>
2024-05-21 20:30:02 -0700

    Base application. Created connection to db, migrated models from db, general views.

---
+++ src/components/PersonPortal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            <h1>Person Portal</h1>
+            <p>Welcome to the portal</p>
+        </div>
+    );
+};
+
+export default MainPage;
