65449dc2915a83370fc22dabe8ccd8901e781a13
Austin3939 <a.blackman39@gmail.com>
2024-11-10 21:20:41 -0500

    deploy 2

--- src/pages/NEWPages/LibraryPage.js
+++ src/pages/NEWPages/LibraryPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import HeaderContent from '../../components/Web/headercontent';
 import { useNavigate } from 'react-router-dom';
+import BookCard from '../../components/Books/BookCard';
 import './LibraryPage.css';
 
 const Books = () => {
